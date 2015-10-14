1ba78f13415d8e5da60e3c873b27b444cc05fb3d
Tomasz Lichon <xorver@gmail.com>
2015-10-14 16:41:47 +0200

    Do not start oneclient in script.

--- install_oneclient.sh
+++ install_oneclient.sh
@@ -4,8 +4,5 @@ ssh -tt $1 fusermount -u ~/fuse
 ssh -tt $1 sudo yum remove -y oneclient
 ssh -tt $1 sudo killall oneclient
 ssh -tt $1 sudo rm -rf ~/.local/share/oneclient/
-ssh -tt $1 sudo yum install -y oneclient
-
-ssh -tt $1 mkdir ~/fuse
-ssh -tt $1 PROVIDER_HOSTNAME=$1 oneclient --authentication token ~/fuse
+ssh -tt $1 sudo yum localinstall -y oneclient-2.6.0-1.el7.centos.x86_64.rpm
 
