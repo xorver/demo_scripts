378fbfd1ed06463afc4e0f8f208bf5e63d67d61b
Tomasz Lichon <xorver@gmail.com>
2015-11-06 18:44:34 +0100

    Remove absolute paths.

--- deploy_packages.sh
+++ deploy_packages.sh
@@ -4,5 +4,5 @@
 ssh -tt gp1 sudo rm -rf /home/oneprovider*.rpm
 ssh -tt gp2 sudo rm -rf /home/oneprovider*.rpm
 
-scp /home/lichon/bamboos/release/build/oneprovider-*el7.centos.x86_64.rpm gp1:~/
-scp /home/lichon/bamboos/release/build/oneprovider-*el7.centos.x86_64.rpm gp2:~/
+scp ~/bamboos/release/build/oneprovider-*el7.centos.x86_64.rpm gp1:~/
+scp ~/bamboos/release/build/oneprovider-*el7.centos.x86_64.rpm gp2:~/
--- reinstall_providers.sh
+++ reinstall_providers.sh
@@ -3,8 +3,8 @@
 ~/bamboos/release/oneprovider_rpm.sh
 
 ./deploy_packages.sh
-scp /home/lichon/demo_scripts/onepanel_config/gp1.cfg gp1:~/
-scp /home/lichon/demo_scripts/onepanel_config/gp2.cfg gp2:~/
+scp ~/demo_scripts/onepanel_config/gp1.cfg gp1:~/
+scp ~/demo_scripts/onepanel_config/gp2.cfg gp2:~/
 
 ./install_provider.sh gp1
 ./install_provider.sh gp2
